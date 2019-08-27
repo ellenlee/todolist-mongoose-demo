eb5bc918b2441099c8a1fbd3c7c79e86fe8c789a
Ellen Lee <leechinghui.tw@gmail.com>
2019-08-27 15:10:18 +0800

    Add connect-flash for UI messages

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -42,6 +43,16 @@ require('./config/passport')(passport)
 app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.isAuthenticated = req.isAuthenticated()
+  next()
+})
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
