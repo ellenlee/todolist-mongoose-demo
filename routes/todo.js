4880796001ac64f919e0f7471c0e148a94892997
Ellen Lee <leechinghui.tw@gmail.com>
2019-08-27 14:45:07 +0800

    Add relations between User and Todo models

--- routes/todo.js
+++ routes/todo.js
@@ -16,7 +16,7 @@ router.get('/new', authenticated, (req, res) => {
 
 // 顯示一筆 Todo 的詳細內容
 router.get('/:id', authenticated, (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
+  Todo.findOne({ _id: req.params.id, userId: req.user._id }, (err, todo) => {
     if (err) return console.error(err)
     return res.render('detail', { todo: todo })
   })
@@ -25,7 +25,8 @@ router.get('/:id', authenticated, (req, res) => {
 // 新增一筆  Todo
 router.post('/', authenticated, (req, res) => {
   const todo = new Todo({
-    name: req.body.name
+    name: req.body.name,
+    userId: req.user._id
   })
 
   todo.save(err => {
@@ -36,7 +37,7 @@ router.post('/', authenticated, (req, res) => {
 
 // 修改 Todo 頁面
 router.get('/:id/edit', authenticated, (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
+  Todo.findOne({ _id: req.params.id, userId: req.user._id }, (err, todo) => {
     if (err) return console.error(err)
     return res.render('edit', { todo: todo })
   })
@@ -44,7 +45,7 @@ router.get('/:id/edit', authenticated, (req, res) => {
 
 // 修改 Todo
 router.put('/:id', authenticated, (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
+  Todo.findOne({ _id: req.params.id, userId: req.user._id }, (err, todo) => {
     if (err) return console.error(err)
     todo.name = req.body.name
     if (req.body.done === 'on') {
@@ -61,7 +62,7 @@ router.put('/:id', authenticated, (req, res) => {
 
 // 刪除 Todo
 router.delete('/:id/', authenticated, (req, res) => {
-  Todo.findById(req.params.id, (err, todo) => {
+  Todo.findOne({ _id: req.params.id, userId: req.user._id }, (err, todo) => {
     if (err) return console.error(err)
     todo.remove(err => {
       if (err) return console.error(err)
