b2de1baee6db36a302e6878c954a68725d1f79b8
Ellen Lee <leechinghui.tw@gmail.com>
2019-08-27 14:01:21 +0800

    Add method-override and update to RESTful routes

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })
 
@@ -70,7 +72,7 @@ app.get('/todos/:id/edit', (req, res) => {
 })
 
 // 修改 Todo
-app.post('/todos/:id', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.name = req.body.name
@@ -87,7 +89,7 @@ app.post('/todos/:id', (req, res) => {
 })
 
 // 刪除 Todo
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id/', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.remove(err => {
